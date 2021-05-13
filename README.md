480b707ed2440013b9177e5c3ab066b89b45b734
Anup Kumar Bid <anupbid94@gmail.com>
2021-05-13 20:02:35 +0530

    Understood Object Transformation

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+// Basic Scene
 1. Scene is like the set where the shooting will take place
 2. Objects are created by two things the shape/geometry and the material with can have many properties, colour being one of them.
 3. Once done we need to add it to the scene.
@@ -6,8 +7,20 @@
 6. We can set position to any object in the scene, because by default evrey thing is added in the center and if the camera is not positioned at a distance from the objects we cannot see anything.
 7. Once the scene it setup we need to render it in the html, using canvas.
 8. We need to provide the canvas DOM reference to WebGL Renderer
-8. WebGL Renderer will resize the canvas based on the scene size once we set the size
-9. Wiew your very first WebGL Render on the HTML.
+9. WebGL Renderer will resize the canvas based on the scene size once we set the size
+10. View your very first WebGL Render on the HTML.
+
+// Transform Objects
+11. Transformation can be done using position, scale, rotation
+11. Position can be added to mesh or camera anything that is inherited from Object3D.
+12. tranformation should be done before the render function has been called, tranforming after the render is done will not work.
+13. AxisHelper helps us to see the axis and position objects in space and passing a unit increases the length of the visible lines.
+14. Functions like lookAt is prest on any Object3D which accepts a Vector3 and it orints the position to look at it, mostly done on camera to make it look at any vector position or a object postion.
+15. We can Group objects together so that applying any tranformation to the group applies it to the whole group.
+16. There are functions like length() which is applied on Object3D objects to get its position from the origin point.
+16. There are functions like distanceTo() which is applied on Object3D objects to get its position beteween two points or objects.
+16. There are functions like normalize() which is applied on Object3D objects to convert vectors into unit vectors.
+
 
 ## Setup
 Download [Node.js](https://nodejs.org/en/download/).
--- src/script.js
+++ src/script.js
@@ -6,13 +6,59 @@ console.log(THREE);
 // Scene
 const scene = new THREE.Scene();
 
-// Red Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1); // 1 unit
-const material = new THREE.MeshBasicMaterial({
-    color: 0xff0000 // 'red' // '#ff0000'
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// Object
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube3.position.x = 2;
+group.add(cube3);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1); // 1 unit
+// const material = new THREE.MeshBasicMaterial({
+//     color: 0xff0000 // 'red' // '#ff0000'
+// });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+// Position 
+// mesh.position.z = 3; // 3 units
+// mesh.position.x = 1;
+// mesh.position.y = 1;
+// mesh.position.set(0.7, -0.6, 1);
+
+// Scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+// mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+// mesh.rotation.reorder('YXZ');
+// mesh.rotation.x = Math.PI * 0.25; // Math.PI is half rotation, *2 is full rotation
+// mesh.rotation.y = Math.PI * 0.25; // Math.PI is half rotation, *2 is full rotation
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(2); // 2 units is the lengthhof the axis line
+scene.add(axesHelper);
 
 // Camera
 const size = {
@@ -22,6 +68,9 @@ const size = {
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height); // degree // Field of View , // aspect ration
 camera.position.z = 3; // 3 units
 scene.add(camera);
+
+// camera.lookAt(new THREE.Vector3(1, 1, 0));
+// camera.lookAt(mesh.position);
 
 // Renderer
 const canvas = document.querySelector('.webgl');
